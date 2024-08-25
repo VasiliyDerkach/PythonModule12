85e158dfdbaa6bb9becfb38a11b2799f7bd0ade4
Vasiliy <v_derkach@inbox.ru>
2024-08-25 18:02:39 +0500

    2

--- tests_12_1.py
+++ tests_12_1.py
@@ -3,21 +3,21 @@ import runner12
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        rn = runner12.Runner('Fake')
-        for i in range(1,10):
-            rn.walk()
-        self.assertEqual(rn.distance,50)
+        rn0 = runner12.Runner('Fake')
+        for i in range(10):
+            rn0.walk()
+        self.assertEqual(rn0.distance,50)
     def test_run(self):
         rn = runner12.Runner('Fake')
-        for i in range(1,10):
+        for i in range(10):
             rn.run()
         self.assertEqual(rn.distance,100)
     def test_challenge(self):
-        rn = runner12.Runner('Fake')
+        rn2 = runner12.Runner('Fake')
         rn1 = runner12.Runner('Lord')
-        for i in range(1,10):
-            rn.run()
-            rn.walk()
-        self.assertNotEqual(rn.distance,rn1.distance)
+        for i in range(10):
+            rn2.run()
+            rn1.walk()
+        self.assertNotEqual(rn2.distance,rn1.distance)
 if __name__=='__main__':
     unittest.main()
